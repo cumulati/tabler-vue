e7d590554c9ea298c244745d5fac3c1c30c80731
Patrick McCarren <patrick@cumulative.io>
2019-04-04 12:34:20 -0400

    feat: allow table rows to be clickable
    Note: only register event handler if needed

---
+++ src/components/Table/TableRow.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- only register click event handler if we have a handler to register -->
+    <tr @click="onClick" v-if="onClick"> <slot/> </tr>
+    <tr v-else> <slot/> </tr>
+</template>
+
+<script>
+export default {
+    name: 'table-row',
+    props: {
+        onClick: {default: false}
+    }
+}
+</script>
