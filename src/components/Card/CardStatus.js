4cd0fd83c5484cf1d431f258bddb166af7820944
Patrick McCarren <patrick@cumulative.io>
2019-04-06 00:21:08 -0400

    feat: card-status-left

--- src/components/Card/CardStatus.js
+++ src/components/Card/CardStatus.js
@@ -1,13 +1,15 @@
 export default {
     name: 'card-status',
     props: {
-        color: {default: 'blue'}
+        color: {default: 'blue'},
+        left: {default: false, type: Boolean},
     },
     template: `<div :class="className"><slot/></div>`,
     computed: {
         className () {
             const className = {'card-status': true}
             className[`bg-${this.color}`] = true
+            className['card-status-left'] = this.left !== false
             return className
         }
     }
